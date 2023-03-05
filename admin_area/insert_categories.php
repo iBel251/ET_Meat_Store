02e2ab89481554ec78221a53d39b45075c7657ed
ibel251 <ibbimran7@gmail.com>
2023-03-04 22:50:41 -0800

    add initial files

---
+++ admin_area/insert_categories.php
@@ -0,0 +1,41 @@
+<?php
+include("../includes/connect.php");
+if(isset($_POST['insert_cat'])){
+    $category_title=$_POST['cat_title'];
+
+    // select data from database
+
+    $select_query="select * from `categories` where category_title ='$category_title'";
+    $result_select=mysqli_query($con,$select_query);
+    $number=mysqli_num_rows($result_select);
+
+    if($number >0){
+      echo "<script>alert('This catagory is present inside the database')</script>";
+    }
+
+    else {
+        $insert_query="insert into `categories`(category_title) values ('$category_title')";
+        $result=mysqli_query($con,$insert_query);
+        if($result){
+            echo "<script>alert('category has been inserted successfully')</script>";
+        }
+    }
+
+}
+?>
+
+<h1 class="text-center">Insert Categories</h1>
+<form action="" method="POST" class="mb-2">
+<div class="input-group mb-2 w-90">
+  <span class="input-group-text bg-info" id="basic-addon1" ><i class="fa-solid fa-receipt">
+
+  </i></span>
+  <input type="text" class="form-control" name="cat_title" placeholder="Insert categories" aria-label="categories" aria-describedby="basic-addon1">
+</div>
+<div class="input-group mb-2 w-10 m-auto">
+
+  <input type="submit" class="bg-info border-0 p-0 my-3" name="insert_cat" value="Insert categories"  aria-label="Username" aria-describedby="basic-addon1">
+
+  <!-- <button class="bg-info p-2 my-3 border-0">Insert Categories</button> -->
+</div>
+</form>
---
+++ mystore.sql
@@ -0,0 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 04, 2023 at 08:23 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mystore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE `brands` (
+  `brand_id` int(11) NOT NULL,
+  `brand_title` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`brand_id`, `brand_title`) VALUES
+(1, 'Guji'),
+(2, 'Harar'),
+(3, 'Sululta'),
+(4, 'kuja'),
+(5, 'Wolega');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `category_title` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `category_title`) VALUES
+(1, 'Fruits'),
+(2, 'Juices');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
