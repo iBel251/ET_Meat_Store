d37de04bc50791c24217f74185d3a9f5551e3313
iBel251 <ibbimran7@gmail.com>
2023-03-08 07:59:10 +0300

    Format update

--- functions/common_function.php
+++ functions/common_function.php
@@ -374,31 +374,29 @@ function total_cart_price()
 }
 
 // get user order details
-function get_user_order_details(){
-global $con;
-$username=$_SESSION['username'];
-$get_details="Select * from `user_table` where username='$username'";
-$result_query=mysqli_query($con,$get_details);
-while($row_query=mysqli_fetch_array($result_query)){
-$user_id=$row_query['user_id'];
-if(!isset($_GEt['edit_account'])){
-  if(!isset($_GEt['my_orders'])){
-    if(!isset($_GEt['delete_account'])){
-     $get_orders="Select * from `user_orders` where user_id=$user_id and order_status='pending'";
-     $result_orders_query=mysqli_query($con,$get_orders);
-     $row_count=mysqli_num_rows($result_orders_query);
-     if($row_count > 0){
-      echo "<h3 class='text-center text-success mt-5 mb-2'>You have <span class='text-danger'>$row_count</span> pending orders </h3>
+function get_user_order_details()
+{
+  global $con;
+  $username = $_SESSION['username'];
+  $get_details = "Select * from `user_table` where username='$username'";
+  $result_query = mysqli_query($con, $get_details);
+  while ($row_query = mysqli_fetch_array($result_query)) {
+    $user_id = $row_query['user_id'];
+    if (!isset($_GEt['edit_account'])) {
+      if (!isset($_GEt['my_orders'])) {
+        if (!isset($_GEt['delete_account'])) {
+          $get_orders = "Select * from `user_orders` where user_id=$user_id and order_status='pending'";
+          $result_orders_query = mysqli_query($con, $get_orders);
+          $row_count = mysqli_num_rows($result_orders_query);
+          if ($row_count > 0) {
+            echo "<h3 class='text-center text-success mt-5 mb-2'>You have <span class='text-danger'>$row_count</span> pending orders </h3>
        <p class='text-center'><a href='profile.php?my_orders'>Order Details</a></p>";
-     } else {
-      echo "<h3 class='text-center text-success mt-5 mb-2'>You have zero pending orders </h3>
+          } else {
+            echo "<h3 class='text-center text-success mt-5 mb-2'>You have zero pending orders </h3>
        <p class='text-center'><a href='../index.php'>Explore products</a></p>";
-     }
+          }
+        }
+      }
     }
   }
 }
-}
-
-}
-
-?>
